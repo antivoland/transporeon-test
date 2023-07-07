d70c4489e698212701d868716f0336cd29b735e4
Vladimir Pavlovskii <antivoland@gmail.com>
2023-07-07 11:17:17 +0300

    init project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Flight Connections
+
+[![Build Status](https://github.com/antivoland/transporeon-test/workflows/build/badge.svg)](https://github.com/antivoland/transporeon-test/actions/workflows/build.yml)
+
+Run the application as follows, for instance:
+
+```shell
+mvn spring-boot:run
+```
---
+++ src/main/java/antivoland/transporeon/FlightConnections.java
@@ -0,0 +1,11 @@
+package antivoland.transporeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightConnections {
+    public static void main(String... args) {
+        SpringApplication.run(FlightConnections.class, args);
+    }
+}
