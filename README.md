1dbcfcc8dd66110d625a5e155d8bf52937d3c6c6
Vladimir Pavlovskii <antivoland@gmail.com>
2023-07-07 12:47:48 +0300

    declare endpoint

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Flight Connections
 
+Check the task description [here](TASK.md).
+
+I [started](https://github.com/antivoland/transporeon-test/tree/typescript) with the implementation using TypeScript, but soon realized that I would not be able to meet the deadline amid other ongoing interview processes. So I've decided to focus on the solution using Java.
+
 [![Build Status](https://github.com/antivoland/transporeon-test/workflows/build/badge.svg)](https://github.com/antivoland/transporeon-test/actions/workflows/build.yml)
 
 Run the application as follows, for instance:
--- src/main/java/antivoland/transporeon/FlightConnections.java
+++ src/main/java/antivoland/transporeon/FlightConnections.java
@@ -2,9 +2,18 @@ package antivoland.transporeon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class FlightConnections {
+    @GetMapping("route")
+    public Object route(@RequestParam("src") String src, @RequestParam("dst") String dst) {
+        return "Not implemented yet";
+    }
+
     public static void main(String... args) {
         SpringApplication.run(FlightConnections.class, args);
     }
