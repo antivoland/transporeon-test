c96e2a83eba29596b9117b5981cba4f61204c583
Vladimir Pavlovskii <antivoland@gmail.com>
2023-07-23 15:51:34 +0300

    filter edge duplicates

--- src/main/java/antivoland/transporeon/model/World.java
+++ src/main/java/antivoland/transporeon/model/World.java
@@ -54,11 +54,11 @@ public class World {
                         && codeMapper.containsKey(route.dstAirportCode)
                         && !route.srcAirportCode.equals(route.dstAirportCode))
                 .forEach(route -> {
-                    // todo: filter duplicates
                     Spot src = spots.get(codeMapper.get(route.srcAirportCode));
                     Spot dst = spots.get(codeMapper.get(route.dstAirportCode));
+                    if (moves.edgeValue(src.id, dst.id).isPresent()) return;
                     Move move = Move.byAir(src, dst, kmDistance(src, dst));
-                    moves.putEdgeValue(move.src.id, move.dst.id, move);
+                    moves.putEdgeValue(src.id, dst.id, move);
                 });
 
         new SegmentationBasedChangeDetector()
