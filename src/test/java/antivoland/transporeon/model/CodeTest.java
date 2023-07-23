2bac15f2d99e707699e82a985852d21159edb945
Vladimir Pavlovskii <antivoland@gmail.com>
2023-07-23 15:47:18 +0300

    codes test

--- src/test/java/antivoland/transporeon/model/CodeTest.java
+++ src/test/java/antivoland/transporeon/model/CodeTest.java
@@ -1,5 +1,8 @@
 package antivoland.transporeon.model;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Set;
@@ -7,8 +10,22 @@ import java.util.Set;
 import static java.util.stream.Collectors.toSet;
 import static org.assertj.core.api.Assertions.assertThat;
 
-// todo: implement
 public class CodeTest {
+    @ParameterizedTest
+    @CsvSource({"TLL,IATA", "tll,IATA", "EETN,ICAO", "eetn,ICAO"})
+    void testValid(String value, Code.Type type) {
+        Code code = Code.code(value);
+        assertThat(code).isNotNull();
+        assertThat(code.type).isEqualTo(type);
+        assertThat(code.value).isEqualTo(value.toUpperCase());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"welcome", "to", "a", "void"}, nullValues = {"void"})
+    void testInvalid(String value) {
+        assertThat(Code.code(value)).isNull();
+    }
+
     public static Set<Code> codes(String... values) {
         Set<Code> codes = Arrays
                 .stream(values)
